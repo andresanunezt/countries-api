d35674c68ca5482cfb450497dd575d46ef840483
Andrés Nuñez <77559024+andresanunezt@users.noreply.github.com>
2021-03-18 11:59:51 -0400

    Update country.rb

--- lib/country.rb
+++ lib/country.rb
@@ -19,4 +19,32 @@ def self.all
 @@all
 end
 
+def self.country_details(country)
+
+    Country.all.each do |countries|
+        if country == countries.name && countries.continent == "Americas"
+        puts "#{countries.name}".colorize(:light_magenta)
+        puts "-----------------------------------------"
+        puts "#{countries.name} is a country in the #{countries.continent}. It has a population of #{countries.population}.".colorize(:green)
+        puts "Details: ".colorize(:light_magenta)
+        puts "Language: #{countries.language}".colorize(:green)
+        puts "Capital: #{countries.capital}".colorize(:green)
+        puts "Native Name: #{countries.native_name}".colorize(:green)
+        puts "Region: #{countries.subregion}".colorize(:green)
+        puts "Currency: #{countries.currency}".colorize(:green)
+        elsif country == countries.name && countries.continent != "Americas"
+            puts "#{countries.name}".colorize(:light_magenta)
+            puts "-----------------------------------------"
+            puts "#{countries.name} is a country in #{countries.continent}. It has a population of #{countries.population}.".colorize(:green)
+            puts "Details: ".colorize(:light_magenta)
+            puts "Language: #{countries.language}".colorize(:green)
+            puts "Capital: #{countries.capital}".colorize(:green)
+            puts "Native Name: #{countries.native_name}".colorize(:green)
+            puts "Region: #{countries.subregion}".colorize(:green)
+            puts "Currency: #{countries.currency}".colorize(:green)
+     end
+    end
+end
+
+
 end
