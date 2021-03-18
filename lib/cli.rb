381d12acc7e5a99721579b445d2195d7252a8a70
Andrés Nuñez <77559024+andresanunezt@users.noreply.github.com>
2021-03-18 12:00:53 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,7 @@ class CLI
 def run
     puts "Welcome to the Country Selector".colorize(:red)
     puts "The World is At Your Fingertips!".colorize(:red)
-    sleep 2
+    sleep 1.5
     API.create_country
     list_countries
     menu
@@ -17,18 +17,51 @@ def list_countries
 end
 
 def menu
+    puts "-----------------------------------------"
     puts "Please select a number between 1 and #{Country.all.size} to see more information:".colorize(:red)
     input = gets.chomp
     if !input.to_i.between?(1, Country.all.count)
-        list_countries
         menu
     else
         country = Country.all[input.to_i-1].name
-        API.country_details(country)
+        Country.country_details(country)
+       sleep 1.5
+        select_other_country
     end
-    
 end
 
+def select_other_country
+puts "-----------------------------------------"
+puts "Would you like to select another country? y/n ".colorize(:red)
+input = gets.chomp
+
+if input.downcase == "y"
+    want_list
+elsif input.downcase == "n"
+    exit_program
+else
+    select_other_country
+end
+end
+
+def want_list
+puts "-----------------------------------------"
+puts "Would you like to see a list of countries? y/n".colorize(:red)
+input = gets.chomp
+if input.downcase == "y"
+    list_countries
+    menu
+elsif input.downcase == "n"
+    menu
+else
+    want_list
+end
+end
+
+def exit_program
+    puts "Goodbye! Come back soon!".colorize(:light_magenta)
+    exit!
+end
 
 
 end
