2f31abbd63d7056ea994778166cd7328392813e8
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-24 12:53:40 -0400

    clean up

--- lib/api.rb
+++ lib/api.rb
@@ -2,8 +2,8 @@ class API
 
 def self.create_country
 response = RestClient.get('https://restcountries.eu/rest/v2/all')
-country_details_array = JSON.parse(response.body, symbolize_names: true)
-country_details_array.collect do |country|
+country_details_hash = JSON.parse(response.body, symbolize_names: true)
+country_details_hash.collect do |country|
     Country.new(country)
 end
 end
